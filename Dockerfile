206c787dc4c904b791cfa95ace495789ee335aa6
George <sonicgd@gmail.com>
2019-05-01 18:29:53 +0500

    custom satisfy container

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM php:7.2-fpm
+
+ENV SATISFY_VERSION=dev-master
+ENV APP_ROOT=/app
+ENV APP_USER=satisfy
+ENV PHP_INI_PATH=/usr/local/etc/php/php.ini
+
+RUN apt update && \
+    apt install -y nginx procmail libxml2-dev inotify-tools jq zip curl openssh-client git gosu
+
+RUN curl https://getcomposer.org/installer -o composer-setup.php && php composer-setup.php --install-dir=/usr/bin --filename=composer && rm composer-setup.php
+
+WORKDIR ${APP_ROOT}
+
+RUN composer create-project sonicgd/satisfy --no-dev . ${SATISFY_VERSION}
+
+RUN rm /usr/local/etc/php-fpm.d/www.conf.default && rm /usr/local/etc/php-fpm.d/www.conf
+COPY conf/php-fpm.conf /usr/local/etc/php-fpm.conf
+
+RUN groupadd ${APP_USER} && useradd -d ${APP_ROOT} -g ${APP_USER} ${APP_USER};
+RUN chown -R ${APP_USER}:${APP_USER} ${APP_ROOT}
+RUN rm ${APP_ROOT}/app/config/parameters.yml
+RUN mkdir ${APP_ROOT}/.ssh
+COPY scripts/*.sh /
+COPY conf/nginx.conf /etc/nginx/nginx.conf
+COPY conf/php.conf /etc/nginx/php.conf
+EXPOSE 80
+
+ENTRYPOINT [ "/entrypoint.sh" ]
+CMD [ "satisfy" ]
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+set -u
+
+GENERATED_SECRET="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
+PARAM_FILE="${APP_ROOT}/app/config/parameters.yml"
+SATIS_FILE="${APP_ROOT}/satis.json"
+
+: ${APP_ROOT:?must be set}
+: ${SECRET:=$GENERATED_SECRET}
+: ${GITLAB_SECRET:=$GENERATED_SECRET}
+: ${GITHUB_SECRET:=$GENERATED_SECRET}
+: ${ADMIN_AUTH:=false}
+: ${ADMIN_USERS:=\~}
+
+: ${SSH_PRIVATE_KEY:=unset}
+: ${ADD_HOST_KEYS:=false}
+: ${STRICT_HOST_KEY_CHECKING:-default set down below}
+
+: ${REPO_NAME:=myrepo}
+: ${HOMEPAGE:=http://localhost:8080}
+
+: ${CRON_ENABLED:=true}
+: ${CRON_SYNC_EVERY:=60}
+
+if [[ ! -e ${PARAM_FILE} ]]; then
+  cat >${PARAM_FILE} <<EOF
+parameters:
+  secret: "${SECRET}"
+  gitlab.secret: "${GITLAB_SECRET}"
+  github.secret: "${GITHUB_SECRET}"
+  satis_filename: "%kernel.project_dir%/satis.json"
+  satis_log_path: "%kernel.project_dir%/var/satis"
+  admin.auth: ${ADMIN_AUTH}
+  admin.users: ${ADMIN_USERS}
+  composer.home: "%kernel.project_dir%/.composer"
+EOF
+fi
+
+
+if [[ "${SSH_PRIVATE_KEY}" != "unset" ]] && [[ ! -e ${APP_ROOT}/id_rsa ]]; then
+  cp ${SSH_PRIVATE_KEY} ${APP_ROOT}/id_rsa
+  chmod 400 ${APP_ROOT}/id_rsa
+  chown ${APP_USER}:${APP_USER} ${APP_ROOT}/id_rsa
+fi
+
+if [[ ! -e ${APP_ROOT}/.ssh/config ]]; then
+  : ${STRICT_HOST_KEY_CHECKING:=no}
+  cat >${APP_ROOT}/.ssh/config <<EOF
+Host *
+IdentityFile ${APP_ROOT}/id_rsa
+StrictHostKeyChecking ${STRICT_HOST_KEY_CHECKING}
+EOF
+chmod 400 ${APP_ROOT}/.ssh/config
+chown ${APP_USER}:${APP_USER} ${APP_ROOT}/.ssh/config
+fi
+
+if [[ ! -e ${SATIS_FILE} ]]; then
+  cat >${SATIS_FILE} <<EOF
+{
+    "name": "${REPO_NAME}",
+    "homepage": "${HOMEPAGE}",
+    "repositories": [
+    ],
+    "require-all": true,
+    "providers": true,
+    "archive": {
+        "directory": "dist",
+        "format": "zip",
+        "skip-dev": false
+    }
+}
+EOF
+    chmod 0777 ${SATIS_FILE}
+fi
+
+if [[ "${CRON_ENABLED}" == "true" ]]; then
+  gosu ${APP_USER}:${APP_USER} /sync_repos.sh ${CRON_SYNC_EVERY}&
+fi
+
+if [[ ${ADD_HOST_KEYS} == "true" ]]; then
+  : ${STRICT_HOST_KEY_CHECKING:=yes}
+  while inotifywait -e close_write ${SATIS_FILE}; do
+    gosu ${APP_USER}:${APP_USER} /record_host_fingerprint.sh
+  done&
+fi
+
+if [[ "${1:-unset}" == "satisfy" ]]; then
+  echo >&2 "Fix access.."
+  chown -R ${APP_USER}:${APP_USER} ${APP_ROOT}/satis.json
+  echo >&2 "Starting Php.."
+  touch /var/log/php-fpm.log
+  php-fpm >/var/log/php-fpm.log 2>&1 &
+  echo >&2 "Starting Nginx.."
+  exec -- nginx
+else
+  exec -- sh
+fi
